5b729a3e3d85dd1247c11868e1634f8a0fb6ffcb
Koto <73934128+Rayykoto@users.noreply.github.com>
2023-11-06 17:09:00 +0700

    Update edit.vue

--- resources/js/Pages/admin/role/edit.vue
+++ resources/js/Pages/admin/role/edit.vue
@@ -60,12 +60,18 @@ export default {
                         <div class="mb-4 text-sm text-slate-500">Input your name</div>
                         <VInput placeholder="Insert Name" label="Name" :required="true" v-model="form.name" />
                     </div>
-                    <div class="w-full">
+                    <br>
+                    <h3 class="mb-1 text-xl font-bold leading-snug text-slate-800">Permissions</h3>
+                    <div class="flex items-center" v-for="(permission, index) in permissions" :key="index">
+                        <input class="rounded form-check-input" type="checkbox" v-model="form.permissions" :value="permission.name" :id="`check-${permission.id}`">
+                        <label class="ml-2 text-black" :for="`check-${permission.id}`">{{ permission.name }}</label>
+                    </div>
+                    <!-- <div class="w-full">
                         <h3 class="mb-1 text-xl font-bold leading-snug text-slate-800">Permissions</h3>
                         <div v-for="(permission, index) in permissions" :key="index" class="mb-4 text-sm text-slate-500">
                             <VCheckbox placeholder="Insert Name" :id="`check-${permission.id}`" :label="permission.name" :required="true" v-model="form.permissions"  />
                        </div>
-                    </div>
+                    </div> -->
             </section>
             <footer>
                 <div class="flex flex-col px-6 py-3 border-t border-slate-200">

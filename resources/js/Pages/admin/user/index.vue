634a3349fe092f326edd05aa68b9a2548e92c711
Koto <73934128+Rayykoto@users.noreply.github.com>
2023-11-06 15:27:43 +0700

    Update index.vue

--- resources/js/Pages/admin/user/index.vue
+++ resources/js/Pages/admin/user/index.vue
@@ -2,13 +2,37 @@
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import AppLayout from '@/layouts/apps.vue';
 import { Inertia } from '@inertiajs/inertia';
+import VBreadcrumb from '@/components/VBreadcrumb/index.vue'
+import VDataTable from '@/components/VDataTable/index.vue';
+import VDropdownEditMenu from '@/components/VDropdownEditMenu/index.vue';
+import VButton from '@/components/VButton/index.vue';
+import VEmpty from '@/components/src/icons/VEmpty.vue';
 export default {
-    layout: AppLayout,
-    components: { Head, Link },
+    layout: AppLayout, 
+    components: { Head, Link, VBreadcrumb, VDataTable, VDropdownEditMenu, VEmpty, VButton },
     props: {
         users: Array
     },
     setup() {
+        const heads = ["Name", "Email", ""];
+        
+        const breadcrumb = [
+            {
+                name: "Dashboard",
+                active: false,
+                to: route('dashboard.index')
+            },
+            {
+                name: "Users",
+                active: false,
+                to: route('users.index')
+            },
+        ];
+
+        const handleAddUser = () => {
+            Inertia.visit(route('users.create'));
+        }
+        
         const deleteItem = (id) => {
             Inertia.delete(`/admin/users/${id}`);
         };
@@ -20,48 +44,54 @@ export default {
         };
         
         return {
-            destroy
+            destroy,
+            heads,
+            handleAddUser,
+            breadcrumb
         }
     }
 }
 </script>
 
 <template>
-    <Head>
+    <Head  />
         <title>Users</title>
-    </Head>
-    <div class="pb-20">
-        <div class="container grid grid-cols-1 p-5 mx-auto sm:w-full md:w-10/12">
-            <div class="flex justify-end mb-2">
-                <Link href="/admin/users/create" class="px-4 py-3 text-xs text-white bg-indigo-600 rounded shadow-sm focus:outline-none">Create</Link>
+        <VBreadcrumb :routes="breadcrumb" />
+
+    <div class="flex items-center justify-between mb-4 sm:mb-6">
+        <h1 class="text-2xl font-bold md:text-3xl text-slate-800">Users</h1>
+    </div>
+
+    <div class="bg-white border rounded-sm shadow-lg border-slate-200"
+        :class="isLoading && 'min-h-[40vh] sm:min-h-[50vh]'">
+        <header class="items-center justify-between block px-4 py-6 sm:flex">
+            <h2 class="font-semibold text-slate-800">
+                All Logs <span class="text-slate-400 !font-medium ml">12</span>
+            </h2>
+            <div class="flex justify-end mt-3 space-x-2 sm:mt-0 sm:justify-between">
+                <VButton label="Add Users" type="primary" @click="handleAddUser"/>
             </div>
-            <div class="p-5 bg-white rounded-md shadow-md">
-                <table class="min-w-full table-auto">
-                    <thead class="justify-between">
-                        <tr class="w-full bg-gray-600">
-                            <th class="px-16 py-2">
-                                <span class="text-white">Name</span>
-                            </th>
-                            <th class="px-16 py-2">
-                                <span class="text-white">Email</span>
-                            </th>
-                            <th class="px-16 py-2">
-                                <span class="text-white">Action</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-gray-200">
-                       <tr v-for="(user, index) in users" :key="index" class="bg-white border">
-                            <td class="justify-center px-16 py-2">{{ user.name }}</td>
-                            <td class="justify-center px-16 py-2">{{ user.email }}</td>
-                            <td class="flex gap-2 px-10 py-3 text-center">
-                                <Link :href="`/admin/users/${user.id}/edit`" class="px-4 py-2 text-xs text-white bg-indigo-600 rounded shadow-sm focus:outline-none">Edit</Link>
-                                <button @click.prevent="destroy(user.id)" class="px-4 py-2 text-xs text-white bg-red-600 rounded shadow-sm focus:outline-none"><i class="fa fa-trash"></i> Delete</button>
-                            </td>
-                       </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        </header>
+
+        <VDataTable :heads="heads" :isLoading="isLoading">
+            <tr v-for="(data, index) in users" :key="index" >
+                <td class="h-16 px-4 whitespace-nowrap"> {{ data.name }}</td>
+                <td class="h-16 px-4 whitespace-nowrap"> {{ data.email }} </td>
+                <td class="h-16 px-4 text-right whitespace-nowrap">
+                    <VDropdownEditMenu class="relative inline-flex r-0" :align="'right'"
+                        :last="index === users.length - 1 ? true : false">
+                        <Link :href="`/admin/users/${data.id}/edit`" class="flex items-center p-3 cursor-pointer hover:bg-slate-100">Edit</Link>
+                        <li class="cursor-pointer hover:bg-slate-100" @click="destroy(data.id)" v-if="!data.is_default">
+                            <div class="flex items-center p-3">
+                                <span>
+                                    <VTrash color="danger" />
+                                </span>
+                                <span>Delete Roles</span>
+                            </div>
+                        </li>
+                    </VDropdownEditMenu>
+                </td>
+            </tr>
+        </VDataTable>
     </div>
 </template>
